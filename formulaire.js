b6a9c5a749d5c6525ec0735da00a60ce5f08e9db
max13 <ren.maxime@gmail.com>
2019-09-12 12:49:39 +0200

    neuvieme commit: finalisation formulaire

--- formulaire.js
+++ formulaire.js
@@ -1,79 +1,125 @@
+var model = {
+    nom: undefined,
+    prenom: undefined,
+    telephone: undefined,
+    email: undefined,
+    isValid: function(){
+        return this.nom && this.prenom && this.telephone && this.email;
+    }
+}
+
 var app = {
     init: function(){
 
-    document.getElementById('nom').addEventListener('blur', validateName);
-    
-    document.getElementById('telephone').addEventListener('blur', validateTel);
+        
+        this.validation();
 
-    document.getElementById('mail').addEventListener('blur', validateMail);
+        this.addListeners();
+    },
 
-    document.getElementById('inscription').addEventListener('submit', validateForm);
+addListeners: function() {
+    var self = this;
+
+    document.getElementById('nom').addEventListener('blur', function(e){
+            var regexName =/[A-Z]{1}[a-z]{1,}/;
+            var erreur = "";
+
+            if(!regexName.test(e.target.value)){
+                erreur ='Vous n avez pas le bon format de nom';
+                model.nom = undefined;
+                e.target.classList.add('styleErreur');
+                e.target.classList.add('span');
+
+            } else {
+                model.nom = e.target.value;
+                e.target.classList.remove('styleErreur');
+                e.target.classList.remove('span');
+            }
+            
+            document.getElementById('aideNom').textContent = erreur;
+            self.validation();
+
+        });
+
+        document.getElementById('prenom').addEventListener('blur', function(e){
+            var regexPrenom =/[a-z]/;
+            var erreur = "";
+
+            if(!regexPrenom.test(e.target.value)){
+                erreur ='Vous n avez pas le bon format de nom';
+                model.prenom = undefined;
+                e.target.classList.add('styleErreur');
+                e.target.classList.add('span');
+            } else {
+                model.prenom = e.target.value;
+                e.target.classList.remove('styleErreur');
+                e.target.classList.remove('span');
+            }
+            
+            document.getElementById('aidePrenom').textContent = erreur;
+            self.validation();
+
+        });
+
+        document.getElementById('telephone').addEventListener('blur', function(e){
+            var regexTel =/[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}/;
+            erreur = "";
+
+            if(!regexTel.test(e.target.value)){
+                erreur ='Vous n avez pas le bon format de numero';
+                model.telephone = undefined;
+                // erreur.classList.add('textErreur');
+                e.target.classList.add('styleErreur');
+                e.target.classList.add('span');
+            } else {
+                model.telephone = e.target.value;
+                e.target.classList.remove('styleErreur');
+                e.target.classList.remove('span');
+            }
+            
+            document.getElementById('aideTel').textContent = erreur;
+            self.validation();
+
+        });
+
+        document.getElementById('mail').addEventListener('blur', function(e){
+            var regexEmail = /.+@.+\..+/;
+            erreur = "";
+
+            if(!regexEmail.test(e.target.value)) {
+                erreur ='Vous n avez pas le bon format d email';
+                document.getElementById('aideMail').textContent = erreur;
+                model.email = undefined;
+                e.target.classList.add('styleErreur');
+                e.target.classList.add('span');
+            } else {
+                model.email = e.target.value;
+                e.target.classList.remove('styleErreur');
+                e.target.classList.remove('span');
+            }
+
+            document.getElementById('aideMail').textContent = erreur;
+            self.validation();
+
+        });
+
+    },
+validation: function(){
+
+    var button = document.getElementById('submit');
+
+        if(model.isValid()){
+            button.disabled = false;
+        } else {
+            button.disabled = true;
+        }
 
     }
+
+            
 }
 
-var erreur;
-
-function validateName(e){
-    var regexName =/[A-Z]{1}[a-z]{1,}/;
-    
-    if(regexName.test(e.target.value)){
-        document.getElementById('nom').style.backgroundColor = 'white';
-        document.getElementById('nom').style.color = 'black';
-        erreur = '';
-        document.getElementById('aideNom').textContent = erreur;
-
-    }else if(!regexName.test(e.target.value)){
-        erreur ='Vous n avez pas le bon format de nom';
-        document.getElementById('aideNom').textContent = erreur;
-        document.getElementById('aideNom').style.color = 'red';
-        document.getElementById('nom').style.backgroundColor = 'red';
-        document.getElementById('nom').style.color = 'white';
-    } 
-}
-
-
-function validateTel(e){
-    var regexTel =/[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}/;
-    
-    if(regexTel.test(e.target.value)){
-        document.getElementById('telephone').style.backgroundColor = 'white';
-        document.getElementById('telephone').style.color = 'black';
-        erreur = '';
-        document.getElementById('aideTel').textContent = erreur;
-    
-    
-    }else if(!regexTel.test(e.target.value)){
-        console.log(validationTel.length);
-        erreur ='Vous n avez pas le bon format de numero';
-        document.getElementById('aideTel').textContent = erreur;
-        document.getElementById('aideTel').style.color = 'red';
-        document.getElementById('telephone').style.backgroundColor = 'red';
-        document.getElementById('telephone').style.color = 'white';
-
-    }
-}
-
-
-function validateMail(e){
-    var regexEmail = /.+@.+\..+/;
-
-    if(regexEmail.test(e.target.value)){
-        document.getElementById('mail').style.backgroundColor = 'white';
-        document.getElementById('mail').style.color = 'black';
-        erreur = '';
-        document.getElementById('aideMail').textContent = erreur;
-    
-    }else if(!regexEmail.test(e.target.value)) {
-        erreur ='Vous n avez pas le bon format d email';
-        document.getElementById('aideMail').textContent = erreur;
-        document.getElementById('aideMail').style.color = 'red';
-        document.getElementById('mail').style.backgroundColor = 'red';
-        document.getElementById('mail').style.color = 'white';
-    }
-
-}
-
+app.init();
 
 function reset(){
     var input = document.getElementsByTagName('input');
@@ -85,12 +131,3 @@ function reset(){
     }
 }
 
-function validateForm(e){
-    if(erreur){
-        e.preventDefault();
-        alert('Votre formulaire doit être valide afin d être envoyé');
-    } else{
-        alert('Formulaire envoyé');
-    }
-}
-    
