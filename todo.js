a41645615b82458d84be82f4e11d8081cb29a767
max13 <ren.maxime@gmail.com>
2019-09-13 14:38:15 +0200

    douzieme commit

--- todo.js
+++ todo.js
@@ -1,87 +1,108 @@
-var tasks = [
-    {
-        id: 1,
-        titre: 'Send email',
-        isDone: false
-    },
-    {
-        id: 2,
-        titre: 'Go to java',
-        isDone: true
-    },
-    {
-        id: 3,
-        titre: 'Make a cake',
-        isDone: true
-    },
-    {
-        id: 4,
-        titre: 'Make a TodoList',
-        isDone: true
-    }
-];
+
 
 
 
 var app = {
     init: function(){
+        this.emptyList();
         
-        
-        this.addCheckbox();
-        this.doneOrNot();
         
     },
-    
-    addCheckbox: function(){
-        var self = this;
+    emptyList: function(){
         
-        for(i = 0; i < tasks.length; i++){
-            addInput();
-            addLabel();
-        }
-        
-    },
+        var inputs = document.getElementsByClassName('box');
 
-    doneOrNot: function(){
+        console.log('compte : ' + inputs.length)
+        if(inputs.length === 0){
+            var emptyMessage = document.createElement('p');
+            emptyMessage.classList.add('empty');
+            emptyMessage.id = 'empty';
+            emptyMessage.innerHTML = 'Votre liste est vide';
+            document.body.appendChild(emptyMessage);
 
-        for(i= 0; i < tasks.length; i++){
-            var inputs = document.getElementsByTagName('input');
-
-            if(tasks[i].isDone == false){
-                inputs[i].checked = false;
-            } else{
-                inputs[i].checked = true;
-            }
-
+        } else if (inputs.length > 0){
+            
+            
         }
     }
+
+}
+
+
+function createNewTask(){
+    var li = document.createElement('li');
+    liste.appendChild(li);
+
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'box';
+    checkbox.name = 'name';
+    checkbox.id = 'id';
+    checkbox.onclick = textBarre;
+    li.appendChild(checkbox);
+    
+    var label = document.createElement('label');
+    var text = document.createTextNode(document.getElementById('myInput').value);
+    label.appendChild(text);
+    li.appendChild(label);
+    
+    var espace = document.createElement('br');
+    liste.appendChild(espace);
 }
 
 
 var liste = document.getElementById("liste");
 
-function addInput(){
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.name = 'name' + tasks[i];
-    checkbox.id = 'id' + tasks[i];
-    liste.appendChild(checkbox);
+function textBarre(){
+    var labels = document.getElementsByTagName('label');
+    var inputs = document.getElementsByClassName('box');
+    
+    for(i = 0; i < inputs.length; i++){
+
+        if(inputs[i].checked == true){
+            labels[i].classList.add('texte-barre');
+           
+        } else {
+            labels[i].classList.remove('texte-barre');
+            
+        }
+    }
+            
+}
+
+
+function supprimer(){
+
+    var li = document.getElementsByTagName('li');
+    var inputs = document.getElementsByClassName('box');
+    
+    var count = li.length;
+
+    for(i = 0; i < count; i++){
+        var suppress = false;
+
+        if(inputs[i].checked === true){
+            suppress = true;
+        } else {
+            suppress = false;
+        }
+
+        if(suppress == true){
+            li[0].remove();
+        }
+            
+            
+            console.log(li.length)
+        
+
+
+    }
 
 }
 
 
-function addLabel(){
-    var label = document.createElement('label');
-    var text = document.createTextNode(tasks[i].titre);
-    label.appendChild(text);
-    liste.appendChild(label);
-    addBr();
 
-}
 
-function addBr(){
-    var espace = document.createElement('br');
-    liste.appendChild(espace);
-}
+
 
 
